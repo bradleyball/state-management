17f897ae5273102f371e53906d6b1658cb1a2cdc
cacheUp <bradb26@gmail.com>
2019-01-28 16:53:42 +0300

    action file

--- flux/pizza-calc/src/actions.js
+++ flux/pizza-calc/src/actions.js
@@ -1,3 +1,19 @@
 import AppDispatcher from "./AppDispatcher";
 
-{type: 'UPDATE_NUMBER_OF_PEOPLE', value: 0 }
+
+
+export const updateNumberOfPeople(value){
+    const action = {type: 'UPDATE_NUMBER_OF_PEOPLE', value: 0 };
+    AppDispatcher.dispatch(action);
+}
+
+export const updateSlicesPerPerson(value) {
+    AppDispatcher.dispatch({
+        type: 'UPDATE_SLICES_PER_PERSON',
+        value
+    })
+}
+
+export const reset(){
+    AppDispatcher.dispatch({type: 'RESET'})
+}
