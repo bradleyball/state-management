67c9155d1a3844c7e6f68d9c3b3dfee4a2d9f6e5
cacheUp <bradb26@gmail.com>
2019-01-28 14:29:50 +0300

    added WithCount hoc component

--- hoc/counter/src/WithCount.js
+++ hoc/counter/src/WithCount.js
@@ -1,1 +1,19 @@
-// Intentionally left blank.
+import React, { Component } from "react";
+
+const WithCount = WrappedComponent =>
+  class extends Component {
+    state = { count: 0 };
+    increment = () => {
+      this.setState(state => ({ count: state.count + 1 }));
+    };
+
+    render() {
+      return (
+        <WrappedComponent
+          count={this.state.count}
+          onIncrement={this.increment}
+          {...this.props}
+        />
+      );
+    }
+  };
